5d1614173a7d960452f6e84c982a59a2890329e3
cowlik <clint@cowlik.com>
2018-10-24 13:48:36 -0700

    added Main; adjusted HeaderNavBtn

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './App.scss';
 import Header from './components/Header';
+import Main from './components/Main';
 
 library.add(faBars);
 
-const App = () => <Header />;
+const App = () => [<Header key="0" />, <Main key="1" />];
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.scss';
+
+const Main = () => (
+    <main className="container">
+        <p>Howdy!</p>
+        <p className="dash">&mdash;</p>
+    </main>
+);
+
+export default Main;
