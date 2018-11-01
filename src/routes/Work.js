1a9f85969a1faf01584adbdf6a3610bc095eec9e
cowlik <clint@cowlik.com>
2018-11-01 16:59:29 -0700

    progress for Work pages

--- src/routes/Work.js
+++ src/routes/Work.js
@@ -6,7 +6,6 @@ import data from '../data/work.json';
 class Work extends Component {
     render() {
         const item = data.find(item => item.slug === this.props.match.params.slug);
-
         return (
             <section className={item.slug}>
                 <header id="work-header">
@@ -19,15 +18,36 @@ class Work extends Component {
                             </h1>
                         </div>
                         <div>
-                            <img src={'/imgs/work/' + item.slug + '/logo.svg'} width="124" height="98" className="logo" alt={item.client} />
+                            <img src={item.logo.path} width={item.logo.width} height={item.logo.height} className="logo" alt={item.client} />
                         </div>
                     </div>
                 </header>
                 <section id="work-content">
-
+                    <div>
+                        <div dangerouslySetInnerHTML={{ __html: item.description }} />
+                        <aside>
+                            <div>
+                                <h3>Launched:</h3>
+                                <p>{item.date}</p>
+                                <h3>Responsibilities:</h3>
+                                <ul>
+                                    {item.responsibilities.map((value, i) => (
+                                        <li key={i}>{value}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>Technologies:</h3>
+                                <ul>
+                                    {item.technologies.map((value, i) => (
+                                        <li key={i}>{value}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </aside>
+                    </div>
                 </section>
             </section>
-
         );
     };
 };
